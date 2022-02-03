e963ea12c69f710d12fdfddc3a6e17c254cbdede
Moises Junior <m.junior@eq.ufrj.br>
2022-02-03 01:25:52 -0300

    refactor: correct error messages and validations

--- index.js
+++ index.js
@@ -29,23 +29,23 @@ const loginSchema = joi.object({
 });
 
 server.post('/sign-up', async (req, res) => {
-    const { name, email, password } = req.body;
-    const validation = registerSchema(req.body);
+    const user = req.body;
+    const validation = registerSchema.validate(user);
 
     if (validation.error) {
-        return res.sendStatus(422);
+        return res.status(422).send("Preencha todos os campos corretamente!");
     }
 
-    const passwordHash = bcrypt.hashSync(password, 10);
+    const passwordHash = bcrypt.hashSync(user.password, 10);
 
     try {
-        const unavailableEmail = await db.collection('users').findOne({ email: email});
+        const unavailableEmail = await db.collection('users').findOne({ email: user.email});
 
         if (unavailableEmail) { 
-            return res.sendStatus(400)
+            return res.status(409).send("Usuário já cadastrado!")
         }
 
-        await db.collection('users').insertOne({...req.body, password: passwordHash});
+        await db.collection('users').insertOne({...user, password: passwordHash});
         res.sendStatus(201)
     } catch {
         res.sendStatus(500)
@@ -54,18 +54,18 @@ server.post('/sign-up', async (req, res) => {
 
 server.post('/sign-in', async (req, res) => {
     const { email, password } = req.body;
-    const validation = loginSchema(req.body);
-    const token;
+    const validation = loginSchema.validate(req.body);
+    let token;
     
     if (validation.error) {
-        return res.sendStatus(422);
+        return res.status(422).send("Preencha todos os campos!");
     }
 
     try {
         const user = await db.collection('users').findOne({ email: email });
 
         if(!user) {
-            return res.sendStatus(401)
+            return res.status(401).send("Usuário não encontrado!")
         };
        
         if (user && bcrypt.compareSync(password, user.password)) {
@@ -73,10 +73,10 @@ server.post('/sign-in', async (req, res) => {
             await db.collection('sessions').insertOne({ token, userID: user._id })
         };
 
-        res.status(201).send(token);
+        res.status(201).send({ token, name: user.name });
     } catch {
         res.sendStatus(500)
     }
-})
+});
 
 server.listen(5000);
