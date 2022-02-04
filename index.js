16ff81f96c3da9da45ed67c085d62699c3540e51
Moises Junior <m.junior@eq.ufrj.br>
2022-02-04 02:19:02 -0300

    feat: get and post on records route

--- index.js
+++ index.js
@@ -28,17 +28,23 @@ const loginSchema = joi.object({
     password: joi.string().required()
 });
 
+const movementSchema = joi.object({
+    value: joi.number().required(),
+    description: joi.string().required(),
+    type: joi.string().required().valid('debt', 'entry'),
+    date: joi.string().required()
+})
+
 server.post('/sign-up', async (req, res) => {
     const user = req.body;
-    const validation = registerSchema.validate(user);
-
-    if (validation.error) {
-        return res.status(422).send("Preencha todos os campos corretamente!");
-    }
-
     const passwordHash = bcrypt.hashSync(user.password, 10);
 
     try {
+        const validation = registerSchema.validate(user);
+        if (validation.error) {
+            return res.status(422).send("Preencha todos os campos corretamente!");
+        }
+
         const unavailableEmail = await db.collection('users').findOne({ email: user.email});
 
         if (unavailableEmail) { 
@@ -54,14 +60,14 @@ server.post('/sign-up', async (req, res) => {
 
 server.post('/sign-in', async (req, res) => {
     const { email, password } = req.body;
-    const validation = loginSchema.validate(req.body);
     let token;
-    
-    if (validation.error) {
-        return res.status(422).send("Preencha todos os campos!");
-    }
 
     try {
+        const validation = loginSchema.validate(req.body);
+        if (validation.error) {
+            return res.status(422).send("Preencha todos os campos!");
+        }
+
         const user = await db.collection('users').findOne({ email: email });
 
         if(!user) {
@@ -79,4 +85,62 @@ server.post('/sign-in', async (req, res) => {
     }
 });
 
+server.get('/records', async (req, res) => {
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+
+    if(!token) {
+        return res.status(401).send("Erro token")
+    };
+
+    try {
+        const session = await db.collection('sessions').findOne({ token });
+
+        if (!session) {
+            return res.status(401).send("Erro sessão")
+        };
+
+        const user = await db.collection('registry').find({ userID: session.userID }).toArray();
+
+        if (!user) {
+            return res.status(401).send("Erro usuário")
+        };
+
+        res.status(201).send(user)  
+    } catch {
+        res.sendStatus(500)
+    }
+});
+
+server.post('/records', async (req, res) => {
+    const { authorization } = req.headers;
+    const token = authorization?.replace('Bearer ', '');
+    const registryData = req.body;
+
+    if(!token) {
+        return res.status(401).send("Erro token")
+    };
+
+     try {
+        const validation = movementSchema.validate(registryData);
+
+        if(validation.error) {
+            return res.status(422).send("Erro de validação")
+        }
+
+        const session = await db.collection('sessions').findOne({ token });
+
+        if (!session) {
+            return res.status(402).send("Erro sessão")
+        };
+
+        await db.collection('registry').insertOne({ ...registryData, userID: session.userID });
+        res.sendStatus(201) 
+    } catch {
+        res.sendStatus(500)
+    }
+})
+
 server.listen(5000);
+
+//mongod --dbpath ~/.mongo
