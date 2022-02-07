039c66bf9d9e95352d13092594d86e160f0be04e
Moises Junior <m.junior@eq.ufrj.br>
2022-02-07 16:32:21 -0300

    removed console.logs

--- src/middlewares/validateSession.js
+++ src/middlewares/validateSession.js
@@ -9,7 +9,6 @@ export default async function validateSession(req, res, next) {
     };
 
     const session = await db.collection('sessions').findOne({ token });
-    console.log(session)
 
     if (!session) {
         return res.status(401).send("Erro sessão")
